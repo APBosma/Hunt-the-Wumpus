230a9ede6ae39b7f8d12173c0ba2fb035d34fa7f
Emilio Bosma <csafety246@gmail.com>
2025-04-28 20:26:49 -0400

    Create main.py
    A start

---
+++ main.py
@@ -0,0 +1,14 @@
+print("Welcome to Hunt the Wumpus!")
+
+while True:
+    choice = input("Type 1 to read the instructions, 2 to play the game, or 3 to quit.")
+    
+    if choice == "1":
+        
+    elif choice == "2":
+        
+    elif choice == "3":
+        print("Goodbye!")
+    else:
+        print("Invalid input, please try again.")
+    
