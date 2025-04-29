f90d813338056117acb04e9832162a446adc9697
Emilio Bosma <csafety246@gmail.com>
2025-04-28 22:00:43 -0400

    Movement
    Implemented movement

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-cave = { "1": ["5", "8", "13"],
+cave = { "1": ["5", "8", "13"], # https://graphonline.top/en/
     "2": ["6", "8", "14"],
     "3": ["13", "17", "16"],
     "4": ["5", "8", "13"],
@@ -18,9 +18,30 @@ cave = { "1": ["5", "8", "13"],
     "18": ["7", "10", "18"],
     "19": ["4", "11", "15"],
     "20": ["14", "16", "18"],
+    "Bats": ["7", "14", "2"],
     "Pitfalls": ["5", "12", "16", "18"],
     "Wumpus": "9"
 }
+
+def printInstructions():
+    print("The wumpus lives in a cave of 20 rooms. Each room has 3 tunnels leading to other rooms. (Look at a dodecahedron to see how this works - if you don't know what a dodecahedron is, ask someone).\n\n")
+    print("Hazards")
+    print("Bottomless pits - two rooms have bottomless pits in them. If you go there, you fall into the pit (& lose!)")
+    print("Super bats - two other rooms have super bats. If you go there, a bat grabs you and takes you to some other room at random. (Which may be troublesome).\n\n")
+    print("Wumpus")
+    print("The wumpus is not bothered by hazards (he has sucker feet and is too big for a bat to lift). Usually he is asleep. Two things wake him up: you shooting an arrow or you entering his room.\n")
+    print("If the wumpus wakes he moves (p=.75) one room or stays still (p=.25). After that, if he is where you are, he eats you up and you lose!\n\n")
+    print("You")
+    print("Each turn you may move or shoot a crooked arrow.")
+    print("Moving: you can move one room (thru one tunnel).")
+    print("Arrows: you have 5 arrows. You lose when you run out. Each arrow can go from 1 to 5 rooms. You aim by telling the computer the rooms you want the arrow to go to. If the arrow can't go that way (if no tunnel) it moves at random to the next room. If the arrow hits the wumpus, you win. If the arrow hits you, you lose.\n\n")
+    print("Warnings")
+    print("When you are one room away from a wumpus or hazard, the computer says:\n")
+    print("Wumpus: \"I smell a wumpus\"\nBat: \"Bats nearby\"\nPit: \"I feel a draft\"\n\n")
+
+def checkRooms(bats, pitfalls, wumpus, possibleRooms):
+    print("Soon to be something")
+    
 
 print("Welcome to Hunt the Wumpus!")
 
@@ -28,23 +49,46 @@ while True:
     choice = input("Type 1 to read the instructions, 2 to play the game, or 3 to quit.\n")
     # Instructions
     if choice == "1":
-        print("The wumpus lives in a cave of 20 rooms. Each room has 3 tunnels leading to other rooms. (Look at a dodecahedron to see how this works - if you don't know what a dodecahedron is, ask someone).\n\n")
-        print("Hazards")
-        print("Bottomless pits - two rooms have bottomless pits in them. If you go there, you fall into the pit (& lose!)")
-        print("Super bats - two other rooms have super bats. If you go there, a bat grabs you and takes you to some other room at random. (Which may be troublesome).\n\n")
-        print("Wumpus")
-        print("The wumpus is not bothered by hazards (he has sucker feet and is too big for a bat to lift). Usually he is asleep. Two things wake him up: you shooting an arrow or you entering his room.\n")
-        print("If the wumpus wakes he moves (p=.75) one room or stays still (p=.25). After that, if he is where you are, he eats you up and you lose!\n\n")
-        print("You")
-        print("Each turn you may move or shoot a crooked arrow.")
-        print("Moving: you can move one room (thru one tunnel).")
-        print("Arrows: you have 5 arrows. You lose when you run out. Each arrow can go from 1 to 5 rooms. You aim by telling the computer the rooms you want the arrow to go to. If the arrow can't go that way (if no tunnel) it moves at random to the next room. If the arrow hits the wumpus, you win. If the arrow hits you, you lose.\n\n")
-        print("Warnings")
-        print("When you are one room away from a wumpus or hazard, the computer says:\n")
-        print("Wumpus: \"I smell a wumpus\"\nBat: \"Bats nearby\"\nPit: \"I feel a draft\"\n\n")
+        printInstructions()
     # Start game
     elif choice == "2":
-        print("Okay")
+        bats = cave["Bats"]
+        pitfalls = cave["Pitfalls"]
+        wumpusLocation = cave["Wumpus"]
+        location = "1"
+        arrows = 5
+        
+        while True:
+            print("\nYou are now in room", location, "of the cave.")
+            move = input("Would you like to shoot, move, or quit? Input S for shoot, M for move, or Q for quit.")
+            if move.upper() == "M":
+                possibleRooms = cave[location]
+                print("You can move to the following rooms:")
+                print(possibleRooms[0], possibleRooms[1], possibleRooms[2])
+                # If bats nearby
+                if (possibleRooms[0] in bats or possibleRooms[1] in bats or possibleRooms[2] in bats):
+                    print("Bats nearby")
+                # If nearby Wumpus
+                if (possibleRooms[0] == wumpusLocation or possibleRooms[1] == wumpusLocation or possibleRooms[2] == wumpusLocation):
+                    print("I smell a wumpus")
+                # If nearby pitfall
+                if (possibleRooms[0] in pitfalls or possibleRooms[1] in pitfalls or possibleRooms[2] in pitfalls):
+                    print("I feel a draft")
+                while True:
+                    newLocation = input("Where would you like to move?")
+                    if (newLocation in possibleRooms):
+                        location = newLocation
+                        break
+                    else:
+                        print("Invalid location. Try again.")
+                
+            elif move.upper() == "S":
+                print("Pew pew")
+            elif move.upper() == "Q":
+                print("Goodbye!")
+                break
+            else:
+                print("Invalid input. Please try again.")
     # Exit
     elif choice == "3":
         print("Goodbye!")
